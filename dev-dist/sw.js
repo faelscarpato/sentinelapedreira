6dd17f4fcf94506205b253e079d5124965f4c997
Scarpato <30941595+faelscarpato@users.noreply.github.com>
2026-04-05 01:27:51 -0300

    Add files via upload

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.6j267gddpk4"
+    "revision": "0.l1g571lvgj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
