1ccda7315a9d18569921fa23b9418fb1958519bb
Scarpato <30941595+faelscarpato@users.noreply.github.com>
2026-04-13 19:56:31 -0300

    Add files via upload

---
+++ dist/assets/sourceUtils-C-cJ2-9f.js
@@ -0,0 +1,1 @@
+function e(r){return r.originalUrl?r.previewMode==="pdf"?!0:/\.pdf(\?|#|$)/i.test(r.originalUrl):!1}function n(r){r&&window.open(r,"_blank","noopener,noreferrer")}export{e as i,n as o};
